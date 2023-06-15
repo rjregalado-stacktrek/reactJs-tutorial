d70445ae5be984ada182e3113d35dec3f74679a6
Robert Regalado <rjregalado1979@gmail.com>
2023-06-15 13:46:53 +0800

    first commit

---
+++ todo-list-app/src/App.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+
+function TodoListApp() {
+  const [tasks, setTasks] = useState([]);
+  const [inputValue, setInputValue] = useState('');
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const addTask = () => {
+    if (inputValue.trim() !== '') {
+      const newTask = {
+        id: Date.now(),
+        text: inputValue,
+        completed: false,
+        priority: 'Low',
+        date: new Date().toLocaleDateString(),
+      };
+      setTasks([...tasks, newTask]);
+      setInputValue('');
+    }
+  };
+
+  const toggleTaskCompletion = (taskId) => {
+    const updatedTasks = tasks.map((task) => {
+      if (task.id === taskId) {
+        return { ...task, completed: !task.completed };
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  };
+
+  const deleteTask = (taskId) => {
+    const updatedTasks = tasks.filter((task) => task.id !== taskId);
+    setTasks(updatedTasks);
+  };
+
+  const filterCompletedTasks = () => {
+    return tasks.filter((task) => !task.completed);
+  };
+
+  const renderTasks = (taskList) => {
+    return taskList.map((task) => (
+      <div key={task.id}>
+        <input
+          type="checkbox"
+          checked={task.completed}
+          onChange={() => toggleTaskCompletion(task.id)}
+        />
+        <span style={{ textDecoration: task.completed ? 'line-through' : 'none' }}>
+          {task.text}
+        </span>
+        <span>({task.priority})</span>
+        <span>{task.date}</span>
+        <button onClick={() => deleteTask(task.id)}>Delete</button>
+      </div>
+    ));
+  };
+
+  return (
+    <div>
+      <h1>ToDo List App</h1>
+      <div>
+        <input type="text" value={inputValue} onChange={handleInputChange} />
+        <button onClick={addTask}>Add Task</button>
+      </div>
+      <div>
+        <h2>All Tasks</h2>
+        {renderTasks(tasks)}
+      </div>
+      <div>
+        <h2>Active Tasks</h2>
+        {renderTasks(filterCompletedTasks())}
+      </div>
+    </div>
+  );
+}
+
+export default TodoListApp;
+
